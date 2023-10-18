3276654fe12f83f5f0802954b251f482b0b21574
jaggergourley <jaggergourley@gmail.com>
2023-10-18 17:27:13 -0500

    Minor changes

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,32 @@
 $(document).ready(function () {
   $("#add-habit-btn").click(function () {
-    const name = $("#name").val();
-    const description = $("#description").val();
+    const name = $("#name").val().trim();
+    const description = $("#description").val().trim();
+
+    if (!name) {
+      alert("Please enter a habit name.");
+      return;
+    }
+
     $.post(
       "/add-habit",
       { name: name, description: description },
       function (data) {
         const habit = data;
         const newHabitHtml = `
-            <li data-id="${habit.id}">
-                ${habit.name} - ${habit.description} (Completed 0 times)
-                <button class="complete-habit-btn">Mark as Completed</button>
-                <button class="delete-habit-btn">Delete</button>
-            </li>
-            `;
+          <li data-id="${habit.id}">
+              ${habit.name} - ${habit.description} (Completed 0 times)
+              <button class="complete-habit-btn">Mark as Completed</button>
+              <button class="delete-habit-btn">Delete</button>
+          </li>
+          `;
         $("#habits-list").append(newHabitHtml);
+        $("#name").val(""); // clear input
+        $("#description").val(""); // clear input
       }
-    );
+    ).fail(function () {
+      alert("Error adding the habit."); // handle error
+    });
   });
 
   $(document).on("click", ".complete-habit-btn", function () {
