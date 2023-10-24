f10204be536005ff56dc0b5b108cdd881db0912f
jaggergourley <jaggergourley@gmail.com>
2023-10-24 17:05:53 -0500

    Refactoring things

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///habits.db'
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
