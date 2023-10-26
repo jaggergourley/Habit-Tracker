e351aff4d9f6dd5a0e3ea2ca998b15a35a860cc2
jaggergourley <jaggergourley@gmail.com>
2023-10-26 16:59:01 -0500

    Dynamically creating table based on days in given month

---
+++ static/js/index.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const habits = ["Program"];
+
+  function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+  }
+
+  function updateTableHeaders(month, year) {
+    console.log("Updating headers..."); // debug log
+    const days = daysInMonth(month, year);
+    const thead = document.querySelector("#habit-tracker thead tr");
+
+    while (thead.children.length > 1) {
+      thead.removeChild(thead.lastChild);
+    }
+
+    for (let day = 1; day <= days; day++) {
+      let th = document.createElement("th");
+      th.textContent = day.toString();
+      thead.appendChild(th);
+    }
+  }
+
+  function populateCalendar(month, year) {
+    console.log("Populating calendar..."); //debug log
+    updateTableHeaders(month, year);
+    const calendarBody = document.querySelector("#habit-tracker tbody");
+    const daysInMonth = new Date(year, month, 0).getDate();
+
+    calendarBody.innerHTML = "";
+
+    for (let day = 1; day <= daysInMonth; day++) {
+      let row = calendarBody.insertRow();
+      for (let habit of habits) {
+        let cell = row.insertCell();
+        cell.textContent = day;
+      }
+    }
+  }
+
+  populateCalendar(10, 2023);
+});
